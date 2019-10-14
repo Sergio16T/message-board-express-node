ce70698e5b6a4373bc7dc6869e5458a16db89271
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-10-13 22:18:19 -0500

    version 1 of mini message board app complete

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+const app = express(); 
+var router = express.Router();
+
+let messages =[
+  {text: "Hi there!", user: "Amando", added: new Date() },
+  {text: "Hello World!", user: "Charles", added: new Date() }, 
+  {text: "Example Message!", user: "Brett", added: new Date() }
+]; 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+router.get('/new', function(req, res, next){
+  res.render('form', {}); 
+});
+
+router.post('/new', function (req, res, next){
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()}); 
+  //res.render('index',{title: 'Mini Message Board', messages: messages}); 
+  res.redirect('/'); 
+})
+
+module.exports = router;
